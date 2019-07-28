ff8d7ee461cb76d5117ae58929a30230fc485e0c
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-27 18:15:58 -0700

    Change dice when dice image is pressed

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,30 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(child: Image.asset('images/dice1.png')),
-        Expanded(child: Image.asset('images/dice2.png')),
-      ],
+    var leftDiceNumber = 1;
+    var rightDiceNumber = 2;
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Left button pressed');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right button pressed');
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
